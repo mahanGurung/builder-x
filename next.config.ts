c52cbe0902b28b1cd6a6a4fdd963dd132df02f26
Mahan Gurung <83807944+mahanGurung@users.noreply.github.com>
2026-01-26 02:09:16 +0545

    feat: Add Vercel deployment configuration

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,22 @@
 import { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.stamp.fyi',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
   experimental: { turbopack: true } as any, // cast entire object
   webpack: (config, { isServer }) => {
     if (!isServer) {
